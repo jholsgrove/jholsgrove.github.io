3e5c51e5b76e39d182a3d5a8e39668fa3127254c
Justin Holsgrove <jholsgrove@highlight.net>
2022-07-01 22:03:46 +0100

    Added darkmode

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,27 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
+const switchElement = document.querySelector('.switch')
+const img = document.getElementById("myImage");
+const darkOrLite = false;
 
 function toggleButton() {
 
     navList.classList.toggle('show')
 }
 
+function changeImage(darkOrLite) {
+      if(img.src !="./images/logo-white.png" && darkOrLite === true)
+      {
+        img.src = "./images/logo-white.png";
+      }
+      else{
+        img.src = "./images/tt-logo.png";
+      };
+}
+
 hamburgerButton.addEventListener('click', toggleButton)
+
+switchElement.addEventListener('click', () => {
+    const darkOrLite = document.body.classList.toggle('dark')
+  changeImage(darkOrLite)
+})
